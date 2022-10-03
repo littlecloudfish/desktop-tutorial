06bd5647ed5a4c4932cbd9177beba7207516b455
littlecloudfish <63266238+littlecloudfish@users.noreply.github.com>
2022-10-03 09:09:18 -0400

    oct3 commit

---
+++ zhengdao-ui/src/views/Music/MusicScoreBoard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="file" @change="onFileSelected">
+        <el-button type="primary" @click="onUploadFile">Upload</el-button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            selectedFile: null,
+        }
+    },
+    methods:{
+        onFileSelected(event){
+            this.selectedFile = event.target.files[0]
+        },
+        onUploadFile(){
+            
+        }
+    },
+
+}
+</script>
