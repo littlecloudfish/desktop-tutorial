ba912b052754ed5b7bb2f3a89fb363e24160198f
littlecloudfish <63266238+littlecloudfish@users.noreply.github.com>
2022-10-06 22:46:28 -0400

    oct6 night commit

--- zhengdao-ui/src/components/layout/picturepost.vue
+++ zhengdao-ui/src/components/layout/picturepost.vue
@@ -1,11 +1,19 @@
 <template>
-    <el-carousel :interval="4000" type="card" height="500px">
+  
+    <el-carousel :interval="4000" type="card" height="500px" >
       <el-carousel-item v-for="item in 4" :key="item">
-        <h3 text="2xl" justify="center">{{ item }}</h3>
+        <h3 text="2xl" justify="center">
+          <el-image style="width: 1000px; height: 500px" src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" fit="fill" />
+        </h3>
       </el-carousel-item>
     </el-carousel>
   </template>
   
+  <script>
+    
+
+  </script>
+
   <style scoped>
   .el-carousel__item h3 {
     color: #475669;
--- zhengdao-ui/src/views/MusicHome.vue
+++ zhengdao-ui/src/views/MusicHome.vue
@@ -2,9 +2,7 @@
   <div class="common-layout">
     <el-container>
       <el-header>
-            <webhead>
-              
-            </webhead> 
+            <webhead></webhead> 
             <picturepost></picturepost>
       </el-header>
       <el-main>
@@ -14,13 +12,17 @@
           </el-aside>
           <el-main>
             <div>
+              <el-main v-if="dataready">
                 <display-music :posts="posts"></display-music>
-                <displayplayer></displayplayer>
+              </el-main>
+                <displayplayer></displayplayer> 
             </div>
           </el-main>
         </el-container>
       </el-main>
-      <el-footer>Footer create mode 100644 zhengdao-ui/node_modules/.cache/babel-loader/f6e4bce43e6d9099b737ca918452a5dc.json</el-footer>
+      <el-footer>
+
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -64,28 +66,37 @@ export default ({
     },
     data() {
         return{
-            posts:[
-                    {
-                        "cover_address": "/demopicture/example.png",
-                        "id": 1,
-                        "music_address": "/DEMOMUSIC/crimanal.mp3",
-                        "name": "crimanal",
-                        "post_date": "2022-08-17T20:13:19",
-                        "user_id": 1
-                    },
-                    {
-                        "cover_address": "/demopicture/example.png",
-                        "id": 2,
-                        "music_address": "/DEMOMUSIC/crimanal.mp3",
-                        "name": "dangerous",
-                        "post_date": "2022-08-17T20:13:19",
-                        "user_id": 2
-                    },]
-
+            // posts:[
+            //         {
+            //             "cover_address": "/demopicture/example.png",
+            //             "id": 1,
+            //             "music_address": "/DEMOMUSIC/crimanal.mp3",
+            //             "name": "crimanal",
+            //             "post_date": "2022-08-17T20:13:19",
+            //             "user_id": 1
+            //         },
+            //         {
+            //             "cover_address": "/demopicture/example.png",
+            //             "id": 2,
+            //             "music_address": "/DEMOMUSIC/crimanal.mp3",
+            //             "name": "dangerous",
+            //             "post_date": "2022-08-17T20:13:19",
+            //             "user_id": 2
+            //         },]
+              posts: null,
+              dataready:false
             
         }
     },
+    async beforeCreate(){
+        this.$store.dispatch('music/musiclistinfo').then(() => {
+            this.posts = this.$store.getters['music/listofMusic'];
+            this.dataready = true;
+        })
+    },
 })
+
+
 </script>
 <style scoped>
 .el-aside{
@@ -94,6 +105,9 @@ export default ({
 .el-main{
   background-color: #0b1c2c;
 
+}
+.el-header{
+  background-color: #0b1c2c;
 }
 .el-header{
   height:600px;
