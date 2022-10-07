50b2157f189d297d9779d2426c1dc8ea6460401a
littlecloudfish <63266238+littlecloudfish@users.noreply.github.com>
2022-10-07 15:48:58 -0400

    oct7 afternoon commit

--- zhengdao-ui/src/views/User/MusicUserUpload.vue
+++ zhengdao-ui/src/views/User/MusicUserUpload.vue
@@ -4,69 +4,51 @@
     <div>
       <el-row>
         <el-col :span="24">Input Music Name<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
+      
         <el-col :span="20">
             <el-input v-model="musicname" placeholder="Please input Music Name" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
     </div>
-    
-    <!-- <div>
+    <div>
       <el-row>
         <el-col :span="24">Input Music Link<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="20">
             <el-input v-model="musiclink" placeholder="Please input Music Link" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
-    </div> -->
-      <!-- <el-row>
+    </div> 
+       <!-- <el-row>
         <el-col :span="24">Input Music Performers(DiscordId)<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="24">
             <el-input v-model="musicperformer" placeholder="Please input Performers" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
       <el-row>
         <el-col :span="24">Input Music Composer(DiscordId)<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="24">
             <el-input v-model="input" placeholder="Please input Composer" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
       <el-row>
         <el-col :span="24">Input Music Editor(DiscordId)<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="24">
             <el-input v-model="input" placeholder="Please input Editor" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
       <el-row>
         <el-col :span="24">Input Music Songwriter(DiscordId)<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="24">
             <el-input v-model="input" placeholder="Please input Songwriter" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="24">Input Music Producer(DiscordId)<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row> 
-      <el-row>
         <el-col :span="24">
             <el-input v-model="input" placeholder="Please input Producer" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>-->
-    <!-- <div>
+      </el-row> -->
+     <div>
       <el-row>
         <el-col :span="24">Input Music Release Date<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="20">
             <el-input v-model="releasedate" placeholder="Please input" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
@@ -75,23 +57,25 @@
     <div>
       <el-row>
         <el-col :span="24">Input Music Lyrics<div class="grid-content ep-bg-purple-dark" /></el-col>
-      </el-row>
-      <el-row>
         <el-col :span="20">
             <el-input v-model="musiclyrics" placeholder="Please Input Lyrics" />
         <div class="grid-content ep-bg-purple-dark" /></el-col>
       </el-row>
 
-    </div> -->
-
-    
-    <div>
-      <input type="file"  @change="onFileSelected">
-      <input type="file" @change="onMusicFileSelected">
     </div>
     <div>
       <el-row>
-        <el-button type="Success" round @click="submitresult" >Submit</el-button>  
+        <el-col :span="24">Input Music Cover Picture<div class="grid-content ep-bg-purple-dark" /></el-col>
+        <input type="file"  @change="onFileSelected">
+      </el-row>
+      <el-row>
+        <el-col :span="24">Input Music Mp3 file<div class="grid-content ep-bg-purple-dark" /></el-col>
+        <input type="file" @change="onMusicFileSelected">
+      </el-row>
+    </div>
+    <div>
+      <el-row>
+        <el-button type="primary" round @click="submitresult" >Submit</el-button>  
       </el-row>
     </div>
   </form>
@@ -141,7 +125,7 @@ export default{
 
 <style lang="scss">
 .el-row {
-  margin-bottom: 20px;
+  margin-bottom: 3px;
 }
 .el-row:last-child {
   margin-bottom: 0;
